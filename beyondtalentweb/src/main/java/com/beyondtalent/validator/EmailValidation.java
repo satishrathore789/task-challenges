5cdb7dc83b9eacec068fcaf06a2b31094a0fb9c2
Admin <Admin@Hp>
2017-09-13 17:40:45 +0530

    set format

--- beyondtalentweb/src/main/java/com/beyondtalent/validator/EmailValidation.java
+++ beyondtalentweb/src/main/java/com/beyondtalent/validator/EmailValidation.java
@@ -4,15 +4,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
+
 @Component
 public class EmailValidation {
 
 	private Pattern pattern;
 	private Matcher matcher;
 
-	private static final String EMAIL_PATTERN =
-		"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
 	public EmailValidation() {
 		pattern = Pattern.compile(EMAIL_PATTERN);
@@ -30,5 +30,5 @@ public class EmailValidation {
 		matcher = pattern.matcher(hex);
 		return matcher.matches();
 	}
-	
+
 }
